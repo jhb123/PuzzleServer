4f0fe267ebd4126fc664f06bf4ad02e1b036272d
jhb123 <jhbriggs23@gmail.com>
2023-06-18 00:15:15 +0100

    adds sending puzzles as zip

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,14 @@
+import json
 import os
+import time
 
 import socketio
 from flask_socketio import SocketIO, send, emit
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, Response, stream_with_context
 from flask_sse import sse
 
 socketio = SocketIO()
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -16,7 +19,15 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    app_dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
     app.config["REDIS_URL"] = "redis://localhost"
+    app.config['ICON_UPLOAD_FOLDER'] = f"{app_dir_path}/uploads/icons"
+    app.config['PUZZLE_UPLOAD_FOLDER'] = f"{app_dir_path}/uploads/puzzles"
+    app.config['ZIP_FOLDER'] = f"{app_dir_path}/uploads/zips"
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -33,9 +44,14 @@ def create_app(test_config=None):
 
     app.register_blueprint(sse, url_prefix='/stream')
 
+
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/test_sockets")
+    def test_sockets():
+        return render_template("test_sockets.html")
 
     @app.route('/test_sse')
     def test_sse():
@@ -50,8 +66,24 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         sse.publish({"message": "Hello!"}, type='greeting')
-        socketio.emit("my response", 'Someone went to /hello')
+        app.logger.info("hello test sent")
+        # socketio.emit("my response", 'Someone went to /hello')
         return 'Hello, World!'
+
+    def get_message():
+        time.sleep(0.1)
+        return time.ctime(time.time())
+
+    @app.route("/testStream")
+    def stream():
+        def event_stream():
+            for i in range(5):
+                time.sleep(0.1)
+                app.logger.info(i)
+                yield f"{i}"
+
+        return stream_with_context(event_stream())
+        # return event_stream(), {"Content-Type": "text/plain"}
 
     from . import db
     db.init_app(app)
@@ -63,6 +95,13 @@ def create_app(test_config=None):
     app.register_blueprint(puzzles.bp)
 
     # socket_app = SocketIO(app)
+
+    @socketio.on('connect')
+    def handle_connection(event):
+        app.logger.info("device connected")
+        app.logger.info(event)
+
+        emit("my response", 'Connected')
 
     @socketio.on('my event')
     def handle_message(event):
--- flaskr/puzzles.py
+++ flaskr/puzzles.py
@@ -1,10 +1,15 @@
 import functools
 import json
+import os
 import sqlite3
+import uuid
+import zipfile
+from io import BytesIO
 
 import flask
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify, app, current_app
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify, app, current_app, Response,
+    send_file
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -26,32 +31,104 @@ def search():
     elif flask.request.method == 'GET':
         puzzle_id = request.args.get('id')
 
+    # puzzle_id = f'{puzzle_id}'
+
     db = get_db()
+    current_app.logger.info(f"getting {puzzle_id}")
 
     puzzle = db.execute(
-        'SELECT puzzle FROM puzzle_table WHERE id = ?', (puzzle_id,)
+        'SELECT * FROM puzzle_table WHERE id = ?', (puzzle_id,)
     ).fetchone()
+    #
+    if(puzzle == None):
+        return "No resource found" , 404
+    current_app.logger.info(puzzle["id"])
 
-    return json.loads(puzzle[0]), 200
+    file_path = os.path.join(current_app.config['ZIP_FOLDER'], f"{puzzle_id}.zip")
+
+    files = [
+        os.path.join(current_app.config['ICON_UPLOAD_FOLDER'], f"{puzzle_id}.png"),
+        os.path.join(current_app.config['PUZZLE_UPLOAD_FOLDER'], f"{puzzle_id}.json")
+    ]
+
+    memory_file = BytesIO()
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        for individualFile in files:
+            # data = zipfile.ZipInfo(individualFile)
+            # data.compress_type = zipfile.ZIP_DEFLATED
+            zf.write(individualFile,individualFile.split("/")[-1])
+        zf.writestr("meta_data.json", json.dumps(dict(puzzle)))
+    memory_file.seek(0)
+    # return "OK",200
+    return send_file(memory_file, download_name='puzzle.zip', mimetype="application/zip")
+
+
 
 @bp.route('/upload', methods=['POST'])
 @token_required
 def upload():
-    id = request.json.get('id')
-    puzzle = request.json.get('puzzle')
+    puzzleFile = request.files['puzzle']
+    imageFile = request.files['image']
+    id = request.form.get("id")
+    time_created = request.form.get("timeCreated")
+    last_modified = request.form.get("lastModified")
 
-    # puzzle = PuzzleData(id,puzzle)
+    current_app.logger.info(id)
+    current_app.logger.info(time_created)
+    current_app.logger.info(last_modified)
 
-    sql = "INSERT INTO puzzle_table VALUES (?,?)"
-
+    sql_query = f"""
+        INSERT OR REPLACE INTO puzzle_table (id, puzzle, timeCreated, lastModified,puzzleIcon)
+        VALUES (?, ?, ?, ? ,?)
+    """
+    data = (id, puzzleFile.filename, time_created, last_modified, imageFile.filename)
     db = get_db()
     try:
-        db.execute(sql, (id, json.dumps(puzzle)))
+        imageFile.save(os.path.join(current_app.config['ICON_UPLOAD_FOLDER'], f"{id}.png"))
+        puzzleFile.save(os.path.join(current_app.config['PUZZLE_UPLOAD_FOLDER'], f"{id}.json"))
+        db.execute(sql_query, data)
         db.commit()
-        return "Created", 201
-    except sqlite3.IntegrityError:
-        # current_app.logger.warning(f"ID not unique")
-        return "ID not unique", 409
+        current_app.logger.info(f"uploaded {id}")
+        return "OK", 200
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return 500
+    # current_app.logger.info(puzzleFile)
+
+    # puzzleDataJson = json.loads(puzzleFile.read())
+    # current_app.logger.info(puzzleDataJson)
+    #
+    # id = puzzleDataJson['id']
+    # puzzle = puzzleDataJson['puzzle']
+    #
+    #
+    # imageFile.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{id}.png"))
+    #
+    # current_app.logger.info(f"{id=}")
+    #
+    # sql_query = f"""
+    #     INSERT OR REPLACE INTO puzzle_table (id, puzzle)
+    #     VALUES (?, ?)
+    # """
+    #
+    # data = (json.dumps(id),json.dumps(puzzle))
+    #
+    # current_app.logger.info(f"uploading {id}")
+    # # return "OK", 200
+    # db = get_db()
+    # try:
+    #     db.execute(sql_query,data)
+    #     db.commit()
+    #     current_app.logger.info(f"uploaded {id}")
+    #     return "Created", 201
+    # except sqlite3.IntegrityError:
+    #     # current_app.logger.warning(f"ID not unique")
+    #     return "ID not unique", 409
 
 
-
+@bp.route('/id')
+@token_required
+def get_guid():
+    guid = uuid.uuid4()
+    return str(guid), 200
