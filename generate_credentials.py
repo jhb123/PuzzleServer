d6acc8ed299ffccb6122fd3b219af89f9c967463
jhb123 <jhbriggs23@gmail.com>
2023-06-19 18:33:30 +0100

    removes old zip file

---
+++ generate_credentials.py
@@ -0,0 +1,26 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+if __name__ == "__main__":
+    scopes = ["https://mail.google.com/"]
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token_2.json'):
+        creds = Credentials.from_authorized_user_file('token_2.json', scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
