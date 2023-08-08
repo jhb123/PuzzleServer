9d1d0a4671e64ca9ecf390db0cc3174a27561ffc
jhb123 <jhbriggs23@gmail.com>
2023-08-08 23:21:51 +0100

    Update conftest.py

--- test/unit_tests/conftest.py
+++ test/unit_tests/conftest.py
@@ -54,6 +54,7 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-north-1"
 
 
 @pytest.fixture
